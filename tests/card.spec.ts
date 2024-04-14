d91dafdbb988c509a97a6dc7d5a3602e423154db
Mariam Laaroussi Ramos <alu0101441764@ull.edu.es>
2024-04-14 23:22:23 +0000

    coverage

--- tests/card.spec.ts
+++ tests/card.spec.ts
@@ -90,4 +90,61 @@ describe('CardCollection', () => {
     const removedCard = cardCollection.getCardById(3);
     expect(removedCard).to.not.exist;
   });
+
+  it('should return undefined when getting a non-existing card', () => {
+    const nonExistingCardId = 999; // ID que no existe en la colección
+    const nonExistingCard = cardCollection.getCardById(nonExistingCardId);
+    expect(nonExistingCard).to.be.undefined;
+  });
+
+  // it('should not add a card with duplicate ID', () => {
+  //   const cardId = 1;
+  //   const card1: Card = new Card(
+  //     cardId,
+  //     'Test Card 1',
+  //     2,
+  //     'Azul',
+  //     'Criatura',
+  //     'Comun',
+  //     'Reglas de prueba 1',
+  //     20
+  //   );
+  //   const card2: Card = new Card(
+  //     cardId, // Mismo ID que card1
+  //     'Test Card 2',
+  //     3,
+  //     'Rojo',
+  //     'Conjuro',
+  //     'Infrecuente',
+  //     'Reglas de prueba 2',
+  //     30
+  //   );
+  //   cardCollection.addCard(card1);
+    
+  //   // Intentar agregar una carta con un ID ya existente debería fallar
+  //   expect(() => cardCollection.addCard(card2)).to.throw('Card with the same ID already exists');
+  // });
+
+  // it('should not update a non-existing card', () => {
+  //   const nonExistingCard: Card = new Card(
+  //     999, // ID que no existe en la colección
+  //     'Non-existing Card',
+  //     4,
+  //     'Negro',
+  //     'Instantaneo',
+  //     'Mítica',
+  //     'Reglas para eliminar',
+  //     50
+  //   );
+
+  //   // Intentar actualizar una carta que no existe en la colección debería fallar
+  //   expect(() => cardCollection.updateCard(nonExistingCard)).to.throw('Card does not exist in the collection');
+  // });
+
+  // it('should not remove a non-existing card', () => {
+  //   const nonExistingCardId = 991; // ID que no existe en la colección
+
+  //   // Intentar eliminar una carta que no existe en la colección debería fallar
+  //   expect(() => cardCollection.removeCard(nonExistingCardId)).to.throw('Card does not exist in the collection');
+  // });
 });
